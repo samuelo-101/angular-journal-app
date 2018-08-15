0f9730e0b5f7ea76ecc6fcd1007df5378acd0acc
Samuel Ojo <samuel@Samuels-MacBook-Pro.local>
2018-08-15 14:51:50 +0200

    Adjusting layout width of card: Was overflowing on mobile devices

--- src/app/view-journal/view-journal.component.ts
+++ src/app/view-journal/view-journal.component.ts
@@ -8,25 +8,20 @@ import { ApiService } from '../service/api.service';
   templateUrl: './view-journal.component.html',
   styleUrls: ['./view-journal.component.css']
 })
-export class ViewJournalComponent implements OnInit, OnDestroy {
+export class ViewJournalComponent implements OnInit {
 
   private _journalId: number;
-  private _routeSubscription: any;
   journal: Journal;
 
   constructor(private _router: Router, private _route: ActivatedRoute, private _apiService: ApiService) { }
 
   ngOnInit() {
-    this._routeSubscription = this._route.params
+    this._route.params
     .subscribe(param => {
       console.log(param);
       this._journalId = param.id;
       this._fetchJournalById();
     });
-  }
-
-  ngOnDestroy() {
-    this._routeSubscription.unsubscribe();
   }
 
   private _fetchJournalById() {
